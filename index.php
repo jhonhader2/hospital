c901b0ffc222eb1e9c70cbfd8bb13dfa0f34e729
Jhon Hader Rodriguez <jhonhader2@hotmail.com>
2023-06-24 16:51:52 -0500

    commit

--- index.php
+++ index.php
@@ -1,158 +1,127 @@
-<?php
+<!doctype html>
+<html lang="es">
 
-//Se tiene una clase “personas” (Mario, Margarita y Alejandro), donde el comportamiento de las personas es leer, trabajar, jugar, etc.; pueden estar en estado despierto o dormido; sus propiedades pueden ser color de ojos, género, estado civil, etc.
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+</head>
 
-$mario = new Persona("Azul", "Masculino", "Viudo");
-echo $mario->getGenero();
-echo "<hr>";
-echo $mario->getEstadoCivil();
-echo "<hr>";
-echo $mario->trabajar();
-echo "<hr>";
-echo $mario->estado(false);
-echo "<hr>";
-echo $mario->getColorOjos();
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            <i class="bi bi-hospital"></i>
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <h1>Formulario de Ingreso de Usuarios</h1>
+        <hr>
+        <div class="row">
+            <form action="" method="post">
+                <div class="row">
+                    <div class="mb-3 col-3">
+                        <label for="primer_nombre" class="form-label">Primer Nombre</label>
+                        <input type="text" class="form-control" id="primer_nombre" aria-describedby="primer_nombre_Help">
+                    </div>
 
-echo "<hr>";
-$Margarita = new Persona("Negros", "Femenino", "Soltera");
-echo "Los ojos de Margarita son de color: ".$Margarita->getColorOjos();
+                    <div class="mb-3 col-3">
+                        <label for="segundo_nombre" class="form-label">Segundo Nombre</label>
+                        <input type="text" class="form-control" id="segundo_nombre" aria-describedby="segundo_nombre_Help">
+                    </div>
 
-class Persona
-{
-    //Propiedades
-    private $color_ojos;
-    private $genero;
-    private $estado_civil;
+                    <div class="mb-3 col-3">
+                        <label for="primer_apellido" class="form-label">Primer Apellido</label>
+                        <input type="text" class="form-control" id="primer_apellido" aria-describedby="primer_apellido_Help">
+                    </div>
+                    <div class="mb-3 col-3">
+                        <label for="segundo_apellido" class="form-label">Segundo Apellido</label>
+                        <input type="text" class="form-control" id="segundo_apellido" aria-describedby="segundo_apellido_Help">
+                    </div>
+                </div>
 
-    public function __construct($color_ojos, $genero, $estado_civil)
-    {
-        $this->color_ojos = $color_ojos;
-        $this->genero = $genero;
-        $this->estado_civil = $estado_civil;
-    }
+                <button type="submit" class="btn btn-primary">Guardar</button>
 
-    //Getters y Setters
-    function getColorOjos()
-    {
-        return $this->color_ojos;
-    }
+            </form>
+        </div>
+    </div>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom">
+        <div class="col-md-4 d-flex align-items-center">
+            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                <i class="bi bi-bootstrap"></i>
+            </a>
+            <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+        </div>
 
-    function getGenero()
-    {
-        return $this->genero;
-    }
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3">
+                <a class="text-body-secondary" href="#">
+                    <i class="bi bi-twitter"></i>
+                </a>
+            </li>
+            <li class="ms-3">
+                <a class="text-body-secondary" href="#">
+                    <i class="bi bi-instagram"></i>
+                </a>
+            </li>
+            <li class="ms-3">
+                <a class="text-body-secondary" href="#">
+                    <i class="bi bi-facebook"></i>
+                </a>
+            </li>
+        </ul>
+    </footer>
 
-    function getEstadoCivil()
-    {
-        return $this->estado_civil;
-    }
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
 
-    //Métodos
-    function leer()
-    {
-        return "Está leyendo";
-    }
-
-    function trabajar()
-    {
-        return "Está trabajando";
-    }
-
-    function jugar()
-    {
-        return "Está jugando";
-    }
-
-    function estado($dato)
-    {
-        if ($dato) {
-            $estado = "Despierto";
-        } else {
-            $estado = "Dormido";
-        }
-
-        return $estado;
-    }
-}
+</html>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function media_aritmetica($a, $b)
+// function media_aritmetica($a, $b) //Aquí definimos la función y la llamamos media_arimetica y las variables a y b
 // {
-//     $media = ($a + $b) / 2;
+// $media = ($a + $b) / 2; //escribimos la fórmula de la media, en este caso de 2 números divididos entre 2
 
-//     return $media;
+// return $media; // aquí la función retorna el valor de la media
 // }
 
-// echo media_aritmetica(7,9);
-// echo "<hr>";
-// echo media_aritmetica(300,500);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Semántica y Sintáxis con PHP
-
-
-
-
-
-//Variables
-// $nombre_completo = "Jhon Hader Rodriguez Perdomo"; //cadena de caracteres
-// $edad = 34; //enteros
-// $temperatura = 23.5; //float
-// $dato_basicos = [
-//     "nombre"    => "Jhon Hader Rodriguez Perdomo",
-//     "edad"      => "34",
-//     "profesion" => "Instructor SENA",
-//]; //arreglo, array
-
-//Funciones 
-// print_r($nombre_completo);
-// echo "<hr>";
-
-// print_r($edad);
-// echo "<hr>";
-
-// print_r($temperatura);
-// echo "<hr>";
-
-// print_r($dato_basicos);
+// echo media_aritmetica(7, 9) . "< br>"; // aquí ingresamos los números, en este caso 7 y 9
+    // echo media_aritmetica(300, 500);//aquí ingresamos otros dos números 300 y 500
